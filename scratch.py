549f2d2045d1792cdd657c2e6f23bf4309910da5
Christopher Fisher <chrisfisher419@gmail.com>
2018-10-24 21:49:15 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -4,3 +4,15 @@ if i < 10:
     print("less")
 else:
     print("more")
+
+    
+    #Fibonnaci
+def Fib(num):
+    a = 0
+    b = 1
+    for l in range(0, num, 1):
+        temp = a
+        a = b
+        b = temp + b
+        print(a)
+        
